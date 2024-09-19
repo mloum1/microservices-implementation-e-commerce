2ff7ba64bf0ea116bc6e3466092f780c1574beeb
mloum1 <maniloum4@gmail.com>
2024-09-19 18:51:59 +0200

    adding projects

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # microservices-implementation-e-commerce
+
+
+docker compose up -d 
+docker compose down 
---
+++ services/microservices-implementation/src/test/java/com/loum/microservices/MicroservicesImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loum.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
